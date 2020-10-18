764ab410081e744e3c975c7c5eddd73ea5b3eee4
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-10-17 22:29:35 -0500

    Updates

---
+++ precache-manifest.c05e8440496fcdf00cff1addd43b121d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c113685f0c50314e3a383674ba4859d7",
+    "url": "/tracker-web/index.html"
+  },
+  {
+    "revision": "dae93fb74ba14da224f8",
+    "url": "/tracker-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ee8b077dd53114309ec",
+    "url": "/tracker-web/static/js/2.2c7b86ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tracker-web/static/js/2.2c7b86ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae93fb74ba14da224f8",
+    "url": "/tracker-web/static/js/main.98f17545.chunk.js"
+  },
+  {
+    "revision": "f43c743b2cd4fc1c887d",
+    "url": "/tracker-web/static/js/runtime-main.fca4e306.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tracker-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
